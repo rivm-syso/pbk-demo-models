d108fb4317bd600c28c020a69e7c6b2f43532226
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2025-02-23 19:18:33 +0100

    Add initial version of simple skin exposure model

--- scripts/compile_models.py
+++ scripts/compile_models.py
@@ -33,39 +33,41 @@ def create_file_logger(logfile: str) -> logging.Logger:
     logger.addHandler(fh)
     return logger
 
-for file in os.listdir(models_path):
-    if file.endswith('.ant'):
-        ant_file = os.path.join(models_path, file)
-        sbml_file = Path(ant_file).with_suffix('.sbml')
+model_folders = [ f.path for f in os.scandir(models_path) if f.is_dir() ]
+for model_path in model_folders:
+    for file in os.listdir(model_path):
+        if file.endswith('.ant'):
+            ant_file = os.path.join(model_path, file)
+            sbml_file = Path(ant_file).with_suffix('.sbml')
 
-        print(f"Creating SBML file [{sbml_file}] from Antimony file [{ant_file}].")
-        r = te.loada(ant_file)
-        r.exportToSBML(sbml_file)
+            print(f"Creating SBML file [{sbml_file}] from Antimony file [{ant_file}].")
+            r = te.loada(ant_file)
+            r.exportToSBML(sbml_file)
 
-        document = ls.readSBML(sbml_file)
+            document = ls.readSBML(sbml_file)
 
-        annotations_file = Path(sbml_file).with_suffix('.annotations.csv')
-        if not os.path.exists(annotations_file):
-            # create annotations (csv) file if it does not exist
-            print(f"Annotations file not found: creating annotations file [{annotations_file}].")
-            model = document.getModel()
-            annotations_template_generator = AnnotationsTemplateGenerator()
-            annotations = annotations_template_generator.generate(model)
-            annotations.to_csv(annotations_file, index=False)
+            annotations_file = Path(sbml_file).with_suffix('.annotations.csv')
+            if not os.path.exists(annotations_file):
+                # create annotations (csv) file if it does not exist
+                print(f"Annotations file not found: creating annotations file [{annotations_file}].")
+                model = document.getModel()
+                annotations_template_generator = AnnotationsTemplateGenerator()
+                annotations = annotations_template_generator.generate(model)
+                annotations.to_csv(annotations_file, index=False)
 
-        annotations_log_file = Path(sbml_file).with_suffix('.annotations.log')
-        annotated_sbml_file = Path(sbml_file).with_suffix('.sbml')
-        annotator = PbkModelAnnotator()
-        logger = create_file_logger(annotations_log_file)
-        print(f"Creating annotated SBML file [{annotated_sbml_file}] from SBML file [{sbml_file}] with annotations file [{annotations_file}].")
-        annotator.annotate(
-            document,
-            annotations_file,
-            logger = logger
-        )
-        ls.writeSBML(document, str(annotated_sbml_file))
+            annotations_log_file = Path(sbml_file).with_suffix('.annotations.log')
+            annotated_sbml_file = Path(sbml_file).with_suffix('.sbml')
+            annotator = PbkModelAnnotator()
+            logger = create_file_logger(annotations_log_file)
+            print(f"Creating annotated SBML file [{annotated_sbml_file}] from SBML file [{sbml_file}] with annotations file [{annotations_file}].")
+            annotator.annotate(
+                document,
+                annotations_file,
+                logger = logger
+            )
+            ls.writeSBML(document, str(annotated_sbml_file))
 
-        validation_log_file = Path(sbml_file).with_suffix('.validation.log')
-        validator = PbkModelValidator()
-        logger = create_file_logger(validation_log_file)
-        validator.validate(annotated_sbml_file, logger)
+            validation_log_file = Path(sbml_file).with_suffix('.validation.log')
+            validator = PbkModelValidator()
+            logger = create_file_logger(validation_log_file)
+            validator.validate(annotated_sbml_file, logger)
